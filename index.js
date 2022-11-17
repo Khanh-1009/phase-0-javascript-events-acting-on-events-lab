6aafad7f1a08959267fdc64ce4d4ce54c82ade8a
Khanh Tran <katetrannbkhanh@gmail.com>
2022-11-16 22:10:15 -0800

    commit tree e94f5600fbbe5533af82705f615cd2734c34c24e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+function moveDodgerLeft(){
+  const leftNumber = dodger.style.left.replace('px', ' ')
+  const left = parseInt(leftNumber,10)
+
+  if (left > 0){
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e){
+  if (e.key === "ArrowLeft"){
+    moveDodgerLeft
+  }
+})
+
+function moveDodgerRight(){
+  const leftNumber = dodger.style.left.replace('px',' ')
+  const left = parseInt(leftNumber, 10)
+
+  if (left < 360){
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e){
+  if (e.key === "ArrowRight"){
+    moveDodgerRight
+  }
+})
